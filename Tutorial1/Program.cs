f638e993c4e71f0174e2c33aaadf274c2b346f22
Kyle Lacy <kylewlacy@me.com>
2013-03-30 15:54:27 -0700

    Tell the user when they don't enter a valid age

--- Tutorial1/Program.cs
+++ Tutorial1/Program.cs
@@ -6,10 +6,22 @@ namespace Tutorial1
 	{
 		public static void Main (string[] args)
 		{
+			int age = 0;
+			string name;
+
 			Console.WriteLine ("What is your name?");
-			string name = Console.ReadLine ();
+			name = Console.ReadLine ();
+
 			Console.WriteLine ("How old are you?");
-			int age = Int32.Parse (Console.ReadLine ());
+
+			string ageInput = Console.ReadLine ();
+			try {
+				age = Int32.Parse (ageInput);
+			} catch {
+				Console.WriteLine (String.Format ("{0} is not a valid age!", ageInput));
+				Console.ReadKey (true);
+				Environment.Exit (0);
+			}
 
 			Console.WriteLine (String.Format ("Hello, {0} (age {1})!", name, age));
 
