68450d1c3cbfc9c822e34dfbc7d511a9c769e484
Kyle Lacy <kylewlacy@me.com>
2013-03-30 16:22:21 -0700

    Add `Person.Greeting()` method

--- Tutorial1/Person.cs
+++ Tutorial1/Person.cs
@@ -10,6 +10,20 @@ namespace Tutorial1
 		public Person ()
 		{
 		}
+
+		public string Greeting() {
+			if(Age < 18) {
+				return "You are too young!";
+			}
+
+			else if(Age < 21) {
+				return "You are old enough, but not old enough for the office parties!";
+			}
+
+			else {
+				return String.Format ("Hello, {0} (age {1})!", Name, Age);
+			}
+		}
 	}
 }
 
--- Tutorial1/Program.cs
+++ Tutorial1/Program.cs
@@ -14,21 +14,7 @@ namespace Tutorial1
 			Console.WriteLine ("How old are you?");
 			newPerson.Age = GetAge();
 
-			if(newPerson.Age < 18) {
-				Console.WriteLine ("You are too young!");
-				Pause ();
-				Environment.Exit (0);
-			}
-
-			else if(newPerson.Age < 21) {
-				Console.WriteLine ("You are old enough, but not old enough for the office parties!");
-				Pause ();
-				Environment.Exit (0);
-			}
-
-			else {
-				Console.WriteLine (String.Format ("Hello, {0} (age {1})!", newPerson.Name, newPerson.Age));
-			}
+			Console.WriteLine (newPerson.Greeting());
 
 			Pause ();
 		}
